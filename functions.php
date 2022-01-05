73c61d339655e3c1997bab44301c3bdf1d7ad48c
aizwellenstan <39293145+aizwellenstan@users.noreply.github.com>
2022-01-05 13:38:12 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,31 @@
+# Add A Discount If Total > amount
+add_filter( 'woocommerce_calculated_total', 'custom_cart_grand_total', 20, 2 );
+function custom_cart_grand_total( $total, $cart ) {
+	if (!(IsUserMember())) {
+		if ($total > 4000) return $total * 0.9;
+	}
+	return $total;
+}
+/*
+function get_current_user_id() {
+    if ( ! function_exists( 'wp_get_current_user' ) ) {
+        return 0;
+    }
+    $user = wp_get_current_user();
+    return ( isset( $user->ID ) ? (int) $user->ID : 0 );
+}
+*/
+
+function IsUserMember() {
+	$user_id = get_current_user_id();
+	$plans = array(3865,3864,3863);
+	foreach ($plans as $plan) {
+		if (wc_memberships_is_user_member( $user_id, $plan ))
+			return true;
+	}
+	return false;
+}
+
 add_action( 'woocommerce_thankyou', 'custom_woocommerce_auto_complete_order');
 function add_plan($user_id, $plan) {
 	if (!wc_memberships_is_user_member( $user_id, $plan )) {
@@ -9,6 +37,8 @@ function add_plan($user_id, $plan) {
 		wc_memberships_create_user_membership( $args );
 	}
 }
+
+# MemberShip
 function custom_woocommerce_auto_complete_order( $order_id ) {
     if ( ! $order_id ) {
         return;
